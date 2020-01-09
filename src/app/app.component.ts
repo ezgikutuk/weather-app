8fc1005e8603899282a0c9a2c651cde7fcb42228
Ezgi Kütük <ezgi.kutuk@derinarge.com>
2020-01-09 16:42:15 +0300

    web servis kullanılarak hava durumu uygulaması yapıldı

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { WeatherService } from './weather.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'havadurumu';
+export class AppComponent implements OnInit {
+  title = 'weather-app';
+
+  constructor(
+    private weatherService: WeatherService
+  ) {
+  }
+
+  weatherResult: any;
+  loading: boolean = true;
+
+  ngOnInit(): void {
+    this.weatherService.getWeather('Adana').subscribe(result => {
+      this.weatherResult = result;
+      console.log(this.weatherResult);
+      this.loading = false;
+    } );
+  }
 }
